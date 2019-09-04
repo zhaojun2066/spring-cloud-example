4ebec147209f1471afd2254207e07f4b02334ecb
zhaojun <zhaojun@300.cn>
2019-09-04 14:04:06 +0800

    spring-cloud-starter-openfeign

---
+++ openfeign-example/openfeign-example-001/src/main/java/com/jufeng/cloud/openfeign001/UserService.java
@@ -0,0 +1,23 @@
+package com.jufeng.cloud.openfeign001;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * @program: spring-cloud-example
+ * @description: 这里可以使用spring controller的 注解
+ * @author: JuFeng(ZhaoJun)
+ * @create: 2019-09-04 09:40
+ **/
+
+@FeignClient(value = "eureka-producer")
+public interface UserService {
+
+    @GetMapping("/user/getName")
+    String getName();
+
+    @GetMapping("/hello")
+    String hello(@RequestParam("what") String what);
+}
